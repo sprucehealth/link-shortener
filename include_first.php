65fafa0b3fc25981e71beb40a5936371692f5eab
David Craig <8527755+dacraig@users.noreply.github.com>
2026-04-29 18:17:32 -0700

    update to match live version

--- include_first.php
+++ include_first.php
@@ -1,4 +1,7 @@
 <?php
+// make sure UTF-8 character set is specified for the page output
+header('Content-Type: text/html; charset=utf-8');
+
 // output debug var to console
 echo "<script>console.log('".$debugvar."');</script>";
 ?>
@@ -6,6 +9,7 @@ echo "<script>console.log('".$debugvar."');</script>";
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<title>Spruce Link Shortener</title>
 		<script src="jquery-3-4-1-min.js"></script>
