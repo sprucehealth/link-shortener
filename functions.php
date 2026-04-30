6c9b15ffdf1cdbbf01e333d3b9e97906eac40bed
David Craig <8527755+dacraig@users.noreply.github.com>
2026-04-29 18:14:04 -0700

    updating to match live version

--- functions.php
+++ functions.php
@@ -12,6 +12,9 @@ function get_my_db() {
 }
 $db = get_my_db();
 
+// set database connection character set to UTF-8
+$db->set_charset("utf8");
+
 // use this helper class to prep a string for db insertion with quotes and escaping
 // in query, instead of saying "field=$var", say "field=$quote->$var"
 class quotefordb {
@@ -27,4 +30,49 @@ $quote = new quotefordb;
 
 // create a variable to hold debug information to be output later
 $debugvar = "";
+
+// function that accepts a URL query string (e.g., "?foo=bar&cat=dog")
+// and returns an array of its various parameters
+// note: this function is useful because the native php $_GET array and parse_str() function both handle potential repeat parameters suboptimally (by overwriting earlier values)
+function proper_parse_str($str) {
+	# result array
+	$arr = array();
+
+	# split on outer delimiter
+	$pairs = explode('&', $str);
+
+	# loop through each pair
+	foreach ($pairs as $i) {
+		# split into name and value
+		# note: these will still be URL-encoded
+		list($name, $value) = explode('=', $i, 2);
+
+		# store the value in an array, using name as the key
+		if (strlen($name) > 0) {
+			$arr[$name][] = $value;
+		}
+
+		/* old way of doing the value storage (single values stored as scalars instead of everything being stored as arrays)
+		# if name already exists
+		if (isset($arr[$name])) {
+			# stick multiple values into an array
+			# if array already exists for this parameter, simply append the new value
+			if (is_array($arr[$name])) {
+				$arr[$name][] = $value;
+			}
+			# else, create an array for this parameter and replace the existing scalar entry with it
+			else {
+				$arr[$name] = array($arr[$name], $value);
+			}
+		}
+		# otherwise, simply stick it in a scalar
+		else {
+			$arr[$name] = $value;
+		}
+		*/
+	}
+
+	# return result array
+ 	return $arr;
+}
 ?>
