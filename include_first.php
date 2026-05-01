3ff41f2dbc99bc52528e9c311304b8f36d2146c2
David Craig <8527755+dacraig@users.noreply.github.com>
2026-04-30 19:15:40 -0700

    editable title and safe use of debugvar

--- include_first.php
+++ include_first.php
@@ -3,7 +3,7 @@
 header('Content-Type: text/html; charset=utf-8');
 
 // output debug var to console
-echo "<script>console.log('".$debugvar."');</script>";
+if (isset($debugvar)) echo "<script>console.log('".$debugvar."');</script>";
 ?>
 
 <html>
@@ -11,7 +11,7 @@ echo "<script>console.log('".$debugvar."');</script>";
 		<meta charset="utf-8">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<link rel="stylesheet" type="text/css" href="style.css">
-		<title>Spruce Link Shortener</title>
+		<title><?php echo $pagetitle ?? 'Spruce Link Shortener' ?></title>
 		<script src="jquery-3-4-1-min.js"></script>
 	</head>
 
